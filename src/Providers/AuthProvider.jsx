2b85c568e0d8f0cfb04741694463d3ae626998df
piasahmed1 <ahmedpias229@gmail.com>
2023-10-31 19:24:39 +0600

    some recent file added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 
@@ -13,6 +13,11 @@ const AuthProvider = ({children}) => {
   const createUser = (email, password) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
   }
 
     useEffect( () => {
@@ -37,7 +42,8 @@ const authInfo = {
   user,
   loading,
   createUser,
-  signIn
+  signIn,
+  logOut
 
 }
 
