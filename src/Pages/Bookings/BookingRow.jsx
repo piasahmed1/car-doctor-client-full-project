72189b8e621dac5b6eb8d63b8af19280b0aefced
piasahmed1 <ahmedpias229@gmail.com>
2023-10-31 23:22:18 +0600

    added some recent file updated

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,7 +1,7 @@
 
-const BookingRow = ({booking, handleDelete}) => {
+const BookingRow = ({booking, handleDelete, handleBookingConfirm}) => {
 
-  const {_id, date, service, price, img} = booking;
+  const {_id, date, service, price, img, status} = booking;
 
   return (
     <tr>
@@ -23,7 +23,9 @@ const BookingRow = ({booking, handleDelete}) => {
       <td>{date}</td>
       <td>${price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        { 
+        status === 'confirm' ? <span className="font-bold text-primary">confirmed</span>:
+          <button onClick={() =>handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>}
       </th>
     </tr>
   );
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -13,7 +13,7 @@ const Bookings = () => {
     fetch(url)
     .then(res => res.json())
     .then(data => setBookings(data))
-  }, [])
+  }, [url])
 
 
   const handleDelete = id => {
@@ -33,6 +33,28 @@ const Bookings = () => {
         }
       })
     }
+  }
+
+  const handleBookingConfirm = id => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({status: 'confirm'})
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.modifiedCount > 0){
+        // Update State
+        const remaining = bookings.filter(booking => booking._id !== id);
+        const updated = bookings.find(bookings => bookings._id === id);
+        updated.status = 'confirm'
+        const newBookings = [updated, ...remaining];
+        setBookings(newBookings);
+      }
+    })
   }
 
 
@@ -63,6 +85,7 @@ const Bookings = () => {
       key={booking._id}
       booking={booking}
       handleDelete = {handleDelete}
+      handleBookingConfirm = {handleBookingConfirm}
       ></BookingRow>)
     }
     </tbody>
